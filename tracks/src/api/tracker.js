6f05e08eda671c9cb503c663e54ae44b9009ef71
K. Dushyant Reddy <dushireddy291@gmail.com>
2021-08-14 23:36:47 +0530

    trck url

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 
 const instance= axios.create({
-    baseURL:'<Your ngrok web URL>',
+    baseURL:'<Your ngrok webURL>',
 });
 
 instance.interceptors.request.use(
